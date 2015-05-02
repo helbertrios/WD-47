e659bf80bc4f87eabc300a45714e3881ce1ed316
web5213 <web5213@caelumBSB01-07>
2015-05-02 10:40:20 -0300

    Sensibiliza o valor total quando altera a quantidade.

---
+++ app/loja/js/carrinho/calcula-valor-total.js
@@ -0,0 +1,15 @@
+var quantidades = document.querySelectorAll("input[id$=quantidade]");
+
+for (var i = 0; i < quantidades.length; i++) {
+    
+    var inputQuantidade = quantidades[i];
+    inputQuantidade.addEventListener("input",  function(evento) {
+        var quantidade = this.value;
+        var li = this.parentNode.parentNode.parentNode;
+        var spanUnitario = li.querySelector("span[id$=unit]");
+        var valorUnitario = realParaNumber(spanUnitario.textContent);
+        var novoTotal = quantidade * valorUnitario;
+        var spanTotal = li.querySelector("span[id$=total]");
+        spanTotal.textContent = numberParaReal(novoTotal);
+    });
+ }
