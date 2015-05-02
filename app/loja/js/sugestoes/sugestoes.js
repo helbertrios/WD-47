4ac7961756c66fe06da80e68b3957c2670c206b6
web5213 <web5213@caelumBSB01-07>
2015-05-02 15:20:39 -0300

    Incluindo sugestões otimizando.

--- app/loja/js/sugestoes/sugestoes.js
+++ app/loja/js/sugestoes/sugestoes.js
@@ -1,12 +1,28 @@
-$("input[type=button]").click( function(event) {
-    console.log("xxxx");
+function adicionarSugestao() {
     var digitado = $("input[type=text]").val();
-    $("<li>")
-        .append($("<span>").addClass("sugestao").text(digitado))
-        .append($("<span>").addClass("votos").text("1 voto"))
-        .appendTo(".sugestoes");    
+    if (digitado != "") {
+        $("<li>")
+            .append($("<span>").addClass("sugestao").text(digitado))
+            .append($("<span>").addClass("votos").text("1 voto"))
+            .appendTo(".sugestoes");
+        $("input[type=text]").val("").focus();
+    }
+}
+
+$("input[type=button]").click( function(event) {
+    adicionarSugestao();
 });
+
+
+$('input[type=text]').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+       adicionarSugestao();
+    }
+});
+
 
 $(".sugestoes li").dblclick(function(event) {
         $(this).remove();
 });
+
