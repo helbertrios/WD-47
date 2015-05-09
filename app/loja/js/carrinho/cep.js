391ca29939e55e822a1dfe7190bb0323052674a1
web5213 <web5213@caelumBSB01-07>
2015-05-09 15:21:18 -0300

    erro consulta cep

--- app/loja/js/carrinho/cep.js
+++ app/loja/js/carrinho/cep.js
@@ -1,8 +1,11 @@
 $("#consulta_cep input[type=submit]").click(function(event) {
+    console.log(" entrou consulta cep");
     var cep = $("#input_cep").val();
     var url = "http://cep.correiocontrol.com.br/"+cep+".json";
     $.getJSON(url, function(dados) {
+        console.log(" entrou consulta getJson");
         var endereco = dados.logradouro+", "+dados.bairro+", "+dados.localidade+", "+dados.uf;
+        console.log(endereco);
         $(".resultado_cep").text(endereco);
     });
     event.preventDefault();
