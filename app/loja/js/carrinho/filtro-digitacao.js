e691a2f5b38dd19e1006d9b4508d356afd870fab
web5213 <andremarquesandrade@gmail.com>
2015-05-09 15:43:51 -0300

    teste

--- app/loja/js/carrinho/filtro-digitacao.js
+++ app/loja/js/carrinho/filtro-digitacao.js
@@ -4,7 +4,9 @@
         var inputsQuantidades =  document.querySelectorAll("#carrinho input");
 
         for (var i = 0; i < inputsQuantidades.length; i++) {
+            
             var quantidade = inputsQuantidades[i];
+            
             quantidade.addEventListener("input", function (event) {
                 var valor = this.value;
                 var valorSemLetra = valor.replace(/\D/g, "");
