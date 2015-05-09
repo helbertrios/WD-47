a13fd55413bbae9d2ef198f5c4497a403ec99396
web5213 <web5213@caelumBSB01-07>
2015-05-09 15:34:21 -0300

    IIFE

--- app/loja/js/carrinho/filtro-digitacao.js
+++ app/loja/js/carrinho/filtro-digitacao.js
@@ -1,11 +1,14 @@
-var inputsQuantidades =  document.querySelectorAll("#carrinho input");
+(
+    function() {
+    var inputsQuantidades =  document.querySelectorAll("#carrinho input");
 
-
-for (var i = 0; i < inputsQuantidades.length; i++) {
-    var quantidade = inputsQuantidades[i];
-    quantidade.addEventListener("input", function (event) {
-        var valor = this.value;
-        var valorSemLetra = valor.replace(/\D/g, "");
-        this.value = valorSemLetra;        
-    });
+    for (var i = 0; i < inputsQuantidades.length; i++) {
+        var quantidade = inputsQuantidades[i];
+        quantidade.addEventListener("input", function (event) {
+            var valor = this.value;
+            var valorSemLetra = valor.replace(/\D/g, "");
+            this.value = valorSemLetra;        
+        });
+    }
 }
+)();
