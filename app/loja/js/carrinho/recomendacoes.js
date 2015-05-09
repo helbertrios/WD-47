c97a0f189b13a6aac27ff0bc4238e12ca754b6a2
web5213 <web5213@caelumBSB01-07>
2015-05-09 10:45:07 -0300

    Produtos

---
+++ app/loja/js/carrinho/recomendacoes.js
@@ -0,0 +1,16 @@
+setInterval(
+    function() {
+        $.getJSON("http://mirrorfashion.caelum.com.br/produtos?callback=?",
+            function(resposta) {
+                var produtos = resposta.produtos;
+                var lista = $("<ul>");
+                $.each(produtos, function () {
+                    var produto = this;
+                        $("<li>")
+                        .append($("<p>").text(produto.nome))
+                        .append($("<p>").text(numberParaReal(produto.preco)))
+                        .appendTo(lista);
+                });
+                $("#recomendacoes").find("ul").remove().end().append(lista);
+            })
+    }, 1000);
