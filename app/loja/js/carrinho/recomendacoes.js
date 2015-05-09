1f5d181a16b5fab5a41b16a163a9e1b7847c287d
web5213 <web5213@caelumBSB01-07>
2015-05-09 11:49:38 -0300

    Refatoramento recomandações

--- app/loja/js/carrinho/recomendacoes.js
+++ app/loja/js/carrinho/recomendacoes.js
@@ -1,21 +1,33 @@
-var buscaRecomendacoes = function() {
-        $.getJSON("http://mirrorfashion.caelum.com.br/produtos?callback=?",
-            function(resposta) {
-                var produtos = resposta.produtos;
-                var lista = $("<ul>");
-                $.each(produtos, function () {
-                    var produto = this;
-                        $("<li>")
-                        .append($("<img>").attr("src", produto.imagem))
-                        .append($("<p>").text(produto.nome))
-                        .append($("<p>").text(numberParaReal(produto.preco)))
-                        .appendTo(lista);
-                });
-                $("#recomendacoes").find("ul").remove().end().append(lista);
-            })
-    }
+
+function trataNovoProduto(produto, lista) {
+        $("<li>")
+        .append($("<img>").attr("src", produto.imagem))
+        .append($("<p>").text(produto.nome))
+        .append($("<p>").text(numberParaReal(produto.preco)))
+        .appendTo(lista);
+}
+
+function trataNovosProdutos(evento, retorno) {
+    var produtos = retorno.produtos;
+    var lista = $("<ul>");
+    $.each(produtos, function(){
+        trataNovoProduto(this, lista);
+    });
+    $("#recomendacoes").find("ul").remove().end().append(lista);
+}
+
+$("#recomendacoes").on("novosProdutos", trataNovosProdutos);
+
+
+function disparaNovosProdutos(retorno){
+    $("#recomendacoes").trigger("novosProdutos", retorno);
+}
+
+function buscaRecomendacoes() {
+        $.getJSON("http://mirrorfashion.caelum.com.br/produtos?callback=?", disparaNovosProdutos);
+}
 
 buscaRecomendacoes();
-setInterval(buscaRecomendacoes(), 5000);
+setInterval(buscaRecomendacoes, 5000);
 
 
