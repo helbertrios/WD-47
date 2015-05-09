9dd639fa838b27ab3bc1c2882545cd9695e96dfe
web5213 <web5213@caelumBSB01-07>
2015-05-09 12:34:33 -0300

    Pesquisa de produtos otimizado

--- app/loja/js/pesquisa/pesquisa.js
+++ app/loja/js/pesquisa/pesquisa.js
@@ -1,13 +1,17 @@
 $(".filtro input").on("input", function (event) {
     var digitado = $(this).val();
     var itens = $("#carrinho li");
-    itens.hide()
-    .find("h2,span")
-    .filter( function() {
-        var conteudo = $(this).text();
-        var regex = new RegExp(digitado, "i");
-        return regex.test(conteudo);
-    })
-    .closest("li")
-    .show();
+    if (digitado) {
+        itens.hide()
+        .find("h2,span")
+        .filter( function() {
+            var conteudo = $(this).text();
+            var regex = new RegExp(digitado, "i");
+            return regex.test(conteudo);
+        })
+        .closest("li")
+        .show();
+    } else {
+        itens.show();
+    }
 });
