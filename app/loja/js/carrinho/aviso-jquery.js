8661ebdee916c38439ada2abc8486d2272b7a703
web5213 <web5213@caelumBSB01-07>
2015-05-09 15:37:38 -0300

    IIFE

--- app/loja/js/carrinho/aviso-jquery.js
+++ app/loja/js/carrinho/aviso-jquery.js
@@ -1,4 +1,8 @@
-$("<button>").attr("id", "botaoaviso").text("Esconder aviso").click( function(event) {        
-        $(".aviso").toggle();
-        $(this).text( $(".aviso").is(":visible") ? "Esconder aviso" : "Mostrar aviso" ) 
-}).insertBefore(".aviso");
+(
+    function () {
+        $("<button>").attr("id", "botaoaviso").text("Esconder aviso").click( function(event) {        
+                $(".aviso").toggle();
+                $(this).text( $(".aviso").is(":visible") ? "Esconder aviso" : "Mostrar aviso" ) 
+        }).insertBefore(".aviso");
+    }
+)();
--- app/loja/js/carrinho/calcula-valor-total.js
+++ app/loja/js/carrinho/calcula-valor-total.js
@@ -1,15 +1,19 @@
-var quantidades = document.querySelectorAll("input[id$=quantidade]");
+(
+    function () {
+        var quantidades = document.querySelectorAll("input[id$=quantidade]");
 
-for (var i = 0; i < quantidades.length; i++) {
-    
-    var inputQuantidade = quantidades[i];
-    inputQuantidade.addEventListener("input",  function(evento) {
-        var quantidade = this.value;
-        var li = this.parentNode.parentNode.parentNode;
-        var spanUnitario = li.querySelector("span[id$=unit]");
-        var valorUnitario = formatador.realParaNumber(spanUnitario.textContent);
-        var novoTotal = quantidade * valorUnitario;
-        var spanTotal = li.querySelector("span[id$=total]");
-        spanTotal.textContent = formatador.numberParaReal(novoTotal);
-    });
- }
+        for (var i = 0; i < quantidades.length; i++) {
+
+            var inputQuantidade = quantidades[i];
+            inputQuantidade.addEventListener("input",  function(evento) {
+                var quantidade = this.value;
+                var li = this.parentNode.parentNode.parentNode;
+                var spanUnitario = li.querySelector("span[id$=unit]");
+                var valorUnitario = formatador.realParaNumber(spanUnitario.textContent);
+                var novoTotal = quantidade * valorUnitario;
+                var spanTotal = li.querySelector("span[id$=total]");
+                spanTotal.textContent = formatador.numberParaReal(novoTotal);
+            });
+         }
+    }
+)();
